0f42e7c1257c3ac43de297a7906274f06e12597d
tejasanerao <tejasanerao75@gmail.com>
2022-05-07 18:02:58 +0530

    Seventh Commit

--- 3 - kNN.py
+++ 3 - kNN.py
@@ -1,3 +1,13 @@
+"""
+
+In the following diagram let blue circles indicate positive examples and orange
+squares indicate negative examples. We want to use k-NN algorithm for classifying
+the points. If k=3, find the class of the point (6,6). Extend the same example for
+Distance-Weighted k-NN and Locally weighted Averaging
+
+"""
+
+
 # import the packages
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
--- 4 - kMean.py
+++ 4 - kMean.py
@@ -1,3 +1,17 @@
+"""
+
+We have given a collection of 8 points. P1=[0.1,0.6] P2=[0.15,0.71] P3=[0.08,0.9]
+P4=[0.16, 0.85] P5=[0.2,0.3] P6=[0.25,0.5] P7=[0.24,0.1] P8=[0.3,0.2]. Perform the
+k-mean clustering with initial centroids as m1=P1=Cluster#1=C1 and
+m2=P8=cluster#2=C2.<br>
+Answer the following<br>
+1] Which cluster does P6 belongs to?<br>
+2] What is the population of cluster around m2?<br>
+3] What is updated value of m1 and m2?
+
+"""
+
+
 #import packages
 from sklearn.cluster import KMeans
 import numpy as np
